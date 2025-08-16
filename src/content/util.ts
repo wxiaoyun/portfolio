faae4cef7cef0462f4cf995cc8abe96215f7cd35
wxiaoyun <w.xy020203@gmail.com>
2025-08-16 11:17:33 +0800

    feat: render draft in dev

--- src/content/util.ts
+++ src/content/util.ts
@@ -1,26 +1,24 @@
 import { getCollection } from "astro:content";
 
+const isDraft = (filePath: string = "") => {
+  const pathParts = filePath.split("/");
+  const filenameParts = pathParts[pathParts.length - 1].split(".");
+  return filenameParts.includes("draft");
+};
+
 export const getSortedBlogEntries = async () => {
   const blogEntries = (await getCollection("blog"))
-    .filter((item) => {
-      if (!item.filePath) {
-        return true;
-      }
-
-      const filePath = item.filePath.split("/");
-      const filenameParts = filePath[filePath.length - 1].split(".");
-      const isDraft = filenameParts.includes("draft");
-
-      if (isDraft) {
-        console.log("Skipping draft: ", item.filePath);
-      }
-
-      return !isDraft;
-    })
+    .filter((item) =>
+      // If in development mode, show all drafts
+      process.env.NODE_ENV === "development" ? true : !isDraft(item.filePath),
+    )
     .map((item) => ({
       ...item,
       data: {
         ...item.data,
+        title: isDraft(item.filePath)
+          ? `[DRAFT] ${item.data.title}`
+          : item.data.title,
         pubDate: new Date(item.data.pubDate),
       },
     }));
