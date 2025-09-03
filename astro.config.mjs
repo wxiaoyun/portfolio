2e138feb0049f67bb162dbb7cc38f073c9a48d9d
wxiaoyun <w.xy020203@gmail.com>
2025-09-03 09:30:23 +0800

    chore: formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
 import remarkToc from "remark-toc";
 
 export default defineConfig({
