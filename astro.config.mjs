9e1b658c0fae2f86021decf6e13389e8d7c658f6
wxiaoyun <w.xy020203@gmail.com>
2025-09-03 09:30:04 +0800

    feat: add footnote and clarification

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
+import remarkGfm from 'remark-gfm'
 import remarkToc from "remark-toc";
 
 export default defineConfig({
@@ -19,7 +20,7 @@ export default defineConfig({
     shikiConfig: {
       theme: "tokyo-night",
     },
-    remarkPlugins: [remarkToc],
+    remarkPlugins: [remarkToc, remarkGfm],
     rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
   },
   integrations: [solidJs({ devtools: true }), mdx()],
